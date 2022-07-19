b1c4c0f6d7fff42fbb64342ff2c3bad48430ae03
movva-abhiram <92386888+movva-abhiram@users.noreply.github.com>
2022-07-19 19:27:42 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,8 @@
+function addUser() {
+
+    user_name = document.getElementById("user_name").Value;
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "Kwitter_room.html";
+}
